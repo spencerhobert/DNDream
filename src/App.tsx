9fedd258ef70e26f551c89593e17b239bf3418fc
Spencer Hobert <spencerhobert@gmail.com>
2025-05-29 23:21:27 -0600

    Added tailwind and react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import { Routes, Route } from 'react-router';
+import CharacterCreator from './pages/CharacterCreator';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  // const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // <>
+    //   <div className="grid grid-cols-2">
+    //     <a href="https://vite.dev" target="_blank">
+    //       <img src={viteLogo} className="logo" alt="Vite logo" />
+    //     </a>
+    //     <a href="https://react.dev" target="_blank">
+    //       <img src={reactLogo} className="logo react" alt="React logo" />
+    //     </a>
+    //   </div>
+    //   <h1>Vite + React</h1>
+    //   <div className="card">
+    //     <button onClick={() => setCount((count) => count + 1)}>
+    //       count is {count}
+    //     </button>
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to test HMR
+    //     </p>
+    //   </div>
+    //   <p className="read-the-docs">
+    //     Click on the Vite and React logos to learn more
+    //   </p>
+    // </>
+    
+    <main className="max-w-6xl mx-auto">
+      <Routes>
+        <Route path="/" element={<CharacterCreator />} />
+        <Route path="*" element={
+          <div className="p-8">
+            <h1 className="text-2xl font-bold text-red-600">
+              404 - Page Not Found
+            </h1>
+          </div>
+        } />
+      </Routes>
+    </main>
+  );
 }
 
 export default App
---
+++ src/pages/CharacterCreator/CharacterCreator.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const CharacterCreator: React.FC = () => {
+    return (
+        <div className="character-creator">
+            <h1>Character Creator</h1>
+            <p>Welcome to the Character Creator page!</p>
+        </div>
+    )
+}
